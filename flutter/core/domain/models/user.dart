c3fd067aa46ac3dad603d2f18a6ce64db97c0df3
Wallisson Rony <wrony.neves@gmail.com>
2021-05-04 20:19:32 -0300

    Remoçao da data de nascimento do model de usuario

--- flutter/core/domain/models/user.dart
+++ flutter/core/domain/models/user.dart
@@ -1,8 +1,7 @@
 class User {
   int id;
   String nome;
-  String dataDeNascimento;
   int idade;
 
-  User(this.id, this.nome, this.dataDeNascimento, this.idade);
+  User(this.id, this.nome, this.idade);
 }
